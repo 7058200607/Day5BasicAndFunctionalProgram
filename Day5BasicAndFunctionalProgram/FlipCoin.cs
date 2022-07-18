c06df0d5f4371ddf16c46897107c65c2337dd0ab
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-07-18 18:44:03 +0530

    Flip the coin and calculate the percentage of heads and tails

---
+++ Day5BasicAndFunctionalProgram/FlipCoin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5BasicAndFunctionalProgram
+{
+    public static class FlipCoin
+    {
+        public static void AFlipCoin()
+        {
+            int head = 0;
+            int tails = 0;
+            int input;
+
+
+            Console.WriteLine("Enter The number of times to Flip Coin :");
+            input = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            if (input > 0)
+            {
+                for (int i = 0; i < input; i++)
+                {
+
+                    if (random.NextDouble() < 0.5)
+                    {
+                        head++;
+                        Console.WriteLine(" Head");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tails");
+                        tails++;
+                    }
+                }
+                double Percentage_head = (double)head / input * 100;
+                Console.WriteLine("percentage of Head :" + Percentage_head + "%");
+                double percentage_tail = (double)tails / input * 100;
+                Console.WriteLine("percentage of tail :" + percentage_tail + "%");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+
+    }
+}
+
---
+++ Day5BasicAndFunctionalProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Day5BasicAndFunctionalProgram
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Choice :");
+            Console.WriteLine("0.Flip the Coin Program");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 0:
+                    FlipCoin.AFlipCoin();
+                    break;
+               
+
+
+            }
+        }
+    }
+}
